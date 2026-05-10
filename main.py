7a14b05c5415dbd6c9c3f61ea494688b86b75131
Thomas <mrtommynell@gmail.com>
2026-05-10 17:53:41 -0500

    init

---
+++ main.py
@@ -0,0 +1,235 @@
+import discord
+from discord.ext import commands, tasks
+import os
+from structure_timers import StructureBot as sb
+from PIL import Image
+import discord
+import requests
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path(".") / ".env"
+
+load_dotenv(dotenv_path=env_path)
+
+#static keys
+timer_channel_id = os.environ.get("channel_id")
+api_key = os.environ.get("discbot")
+timer_response_channel = int(os.environ.get("CHANNEL_ID"))
+timer_dict_glob = {}
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+def get_old_timers(file_path):
+  try: 
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split(':')
+            if len(parts) == 2:
+              timer_dict_glob.update({int(parts[0]):parts[1]})
+        file.close()
+  except:
+    pass
+
+@bot.event
+async def on_ready():
+  print('the bot is ready')
+  remove_expired_timers.start()
+  response_channel = bot.get_channel(timer_response_channel)
+  file_path = 'timers.txt'
+  get_old_timers(file_path)
+  sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+  timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+  await response_channel.purge(limit=5)
+  await response_channel.send(timers_msg)
+  
+@bot.command()
+async def t(ctx, *, time):
+    '''
+    creates a timer for a structure in a specified channel
+    '''
+    response_channel = bot.get_channel(timer_response_channel)
+    timers_data = sb.timer(time)
+    timer_dict_glob.update({timers_data[1]: timers_data[0]})
+    #sort the timers and retrieve from the dictionary
+    sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+    timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+    txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+    sb.write_to_timers_txt(txt_msg)
+    try:
+      await response_channel.purge(limit=2)
+      await response_channel.send(timers_msg)
+    except:
+      await response_channel.purge(limit=2)
+      await response_channel.send(timers_msg)
+
+@bot.command()
+async def timer(ctx, *args):
+  
+    response_channel = bot.get_channel(timer_response_channel)
+    user = ctx.message.author
+  
+    # Check if an image is attached to the message
+    if len(ctx.message.attachments) == 0:
+        await ctx.send("Please attach an image to the command.",  delete_after=40)
+        return
+
+    # Get the first attached image
+    image_url = ctx.message.attachments[0].url
+    timer_args = ' '.join(args)
+
+    # Download and save the image
+    image = Image.open(requests.get(image_url, stream=True).raw)
+    image.save("saved_image.png")
+    try:
+      results = sb.read_img("saved_image.png")
+      parsed_datetime = sb.date_from_list(results)
+      unix_ts = sb.to_unix_time(parsed_datetime)
+      timer_dict_glob.update({unix_ts: timer_args})
+      try: 
+        #sort the timers and retrieve from the dictionary
+        sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+        timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+        txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+        sb.write_to_timers_txt(txt_msg)
+        try:
+          await response_channel.purge(limit=2)
+          await response_channel.send(timers_msg)
+        except:
+          await response_channel.send(timers_msg)
+      except:
+        await ctx.send(results)
+    except:
+      await ctx.send(f'Cant read the date plz add manualy with !t command {user.mention}',delete_after=40)
+
+@bot.command()
+async def td(ctx, str_nm="", days=0, hours=0, minutes=0):
+    response_channel = bot.get_channel(timer_response_channel)
+    user = ctx.message.author
+    try:
+          # Convert days, hours, and minutes to seconds
+          delta_seconds = (days * 86400) + (hours * 3600) + (minutes * 60)
+          # Get the current UTC time
+          utc_now = datetime.utcnow()
+          # Calculate the UTC epoch timestamp by adding the delta to the current time
+          utc_epoch_ts = int((utc_now + timedelta(seconds=delta_seconds)).timestamp())
+          timer_dict_glob.update({utc_epoch_ts: str_nm})
+          sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+          timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+          txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+          sb.write_to_timers_txt(txt_msg)
+          try:
+            await response_channel.purge(limit=2)
+            await response_channel.send(timers_msg)
+          except:
+            pass
+
+    except ValueError:
+          await ctx.send(f'this command needs <structure name> <days> <hours> <minutes>{user.mention}',delete_after=40)
+
+@bot.command()
+async def bulk_timer(ctx, *args):
+  
+    response_channel = bot.get_channel(timer_response_channel)
+    
+  
+    # Check if an image is attached to the message
+    if len(ctx.message.attachments) == 0:
+        await ctx.send("Please attach an image to the command.",  delete_after=40)
+        return
+
+    # Get the first attached image
+    attachment = ctx.message.attachments[0]
+    url = attachment.url
+    response = requests.get(url)
+    if response.status_code == 200:
+      with open('bulk.txt', 'wb') as file:
+          file.write(response.content)
+          file.close()
+    
+    with open('bulk.txt', 'r') as file:
+      for line in file:
+        parts = line.strip().split(':')
+        if len(parts) == 2:
+          timer_dict_glob.update({int(parts[0]):parts[1]})
+    file.close()
+        
+    
+    #sort the timers and retrieve from the dictionary
+    try: 
+      sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+      txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+      sb.write_to_timers_txt(txt_msg)
+      try:
+        await response_channel.purge(limit=2)
+        await ctx.send('adding timers')
+        #await response_channel.send(timers_msg)
+      except:
+        await ctx.send('adding timers', delete_after=40)
+    except:
+      await ctx.send('add it the hard way',delete_after=40)
+      
+@tasks.loop(seconds=5)
+async def remove_expired_timers():
+    response_channel = bot.get_channel(timer_response_channel)
+
+    # Get the current UTC time
+    current_unix_time = sb.unix_time_now()
+
+    # Create a copy of keys to remove
+    keys_to_remove = [key for key in timer_dict_glob if (key + 3600) < current_unix_time]
+
+    # Remove the keys with timestamps that have passed
+    for key in keys_to_remove:
+        del timer_dict_glob[key]
+
+    if keys_to_remove:
+        # If keys were removed, send a message and purge the channel
+        await response_channel.purge(limit=2)
+        sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+        timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+        txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+        sb.write_to_timers_txt(txt_msg)
+
+        if not timer_dict_glob:
+            # If the dictionary is empty, send a default message
+            default_msg = "There are no active timers."
+            await response_channel.send(content=default_msg)
+        else:
+            # Sort the remaining timers by timestamp
+            sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+
+            # Create a message with the remaining timers
+            timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+            txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+            sb.write_to_timers_txt(txt_msg)
+            await response_channel.send(timers_msg)
+
+@bot.command()
+async def rem(ctx, key):
+    key = int(key)
+    response_channel = bot.get_channel(timer_response_channel)
+    if key in timer_dict_glob:
+        # Delete the timer associated with the given key
+        del timer_dict_glob[key]
+        await ctx.send(f'Timer with key "{key}" has been removed.')
+        sorted_timers = dict(sorted(timer_dict_glob.items(), reverse=True))
+        timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+        txt_msg = '\n'.join([f'{key}:{value}'for key,value in sorted_timers.items()])
+        sb.write_to_timers_txt(txt_msg)
+        await response_channel.purge(limit=5)
+        try: 
+          await response_channel.send(timers_msg)
+        except:
+          await response_channel.send('There are no active timers.')
+    else:
+        await ctx.send(f'Timer with key "{key}" not found.')
+
+
+def main():
+    
+    bot.run(api_key)
+
+if __name__ == "__main__":
+    main()
---
+++ structure_timers.py
@@ -0,0 +1,124 @@
+import os
+from datetime import datetime, timezone
+import easyocr
+import re
+
+class StructureBot:
+    
+    def __init__() -> None:
+        pass
+    
+    def get_channel_id(channel_name):
+        """
+        gets the channel id from env file
+        """
+        channel_id = os.environ.get(f"{channel_name}")
+        return int(channel_id)
+    
+    def to_unix_time(date_time):
+        unix_time = int(datetime.timestamp(date_time))
+        return unix_time
+
+    def timer(time):
+        """
+        given a string that looks like text, text and time stamp split on the "," parse the list to find the timestamp convert the timestamp into short and relitive date format
+        for discod and return a tuple with all the desired values
+        take curr unix time - future unix time + 900 (15 minm)
+        """
+        pattern = r'(\d{4}[.-]\d{2}[.-]\d{2} \d{2}[.:]\d{2}[.:]\d{2})'
+
+        # Search for the pattern in the message
+        match = re.search(pattern, time)
+
+        if match:
+            # Extract the matched date and time string
+            date_time_str = match.group(1)
+
+            # Convert the date and time string to a datetime object
+            date_time_str = re.sub(r'[-.]', '.', date_time_str)
+            dt_structure_timer = datetime.strptime(date_time_str, '%Y.%m.%d %H:%M:%S')
+            unix_structure_timer = StructureBot.to_unix_time(dt_structure_timer)
+            # Remove the date and time portion from the message
+            cleaned_message = re.sub(pattern, '', time).strip()
+
+            return (cleaned_message, unix_structure_timer)
+        else:
+            return print('f')
+    
+    def date_from_list(ocr_results):
+        for i in ocr_results:
+            # Use re.search to find the date and time pattern in the string
+            match = re.search(r'\d{4}\.\d{2}\.\d{2} \d{2}\.\d{2}', i)
+            if match:
+                # Extract the matched pattern and parse it into a datetime object
+                datetime_str = match.group()
+                datetime_format = '%Y.%m.%d %H.%M'
+                try:
+                    parsed_datetime = datetime.strptime(datetime_str, datetime_format)
+                    return parsed_datetime
+                except ValueError:
+                    # Handle ValueError in case of an invalid datetime format
+                    pass
+
+        return None
+
+    def read_img(image_path):
+        reader = easyocr.Reader(['en'], gpu = False)
+        preprocessed_image = image_path
+        results = reader.readtext(preprocessed_image, detail=0)
+        return results
+    
+    def ocr_timer(timer):
+        unix_structure_timer = StructureBot.to_unix_time(timer)
+        current_unix_time = StructureBot.unix_time_now()
+        seconds_till_timer = unix_structure_timer - current_unix_time + 900
+        return seconds_till_timer
+
+    def find_timers_txt(file_path):
+        result = ''
+        try:
+            with open(file_path, 'r') as file:
+            # Iterate through each line in the file
+                for line in file:
+                    # Split the line on ":"
+                    parts = line.strip().split(':')
+                    if len(parts) == 2:
+                        # Add the two parts separated by ":" to the result
+                        result += f"> {parts[1]} <t:{parts[0]}:f> in <t:{parts[0]}:R>\n"
+            return result
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            pass 
+        
+    def find_time_left(file_path):
+        try:
+            t = StructureBot.find_timers_txt(file_path)
+            current_unix_time = StructureBot.unix_time_now()
+            timers = t.split('\n')
+            tc = timers[-1].split(':')
+            
+            time_left = int(tc[1]) - current_unix_time
+            return time_left
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            pass 
+        
+    def unix_time_now():
+        current_unix_time = StructureBot.to_unix_time(datetime.now(timezone.utc))
+        return current_unix_time
+    
+    def timer_to_dict(timer_map, results, timer_args):
+        parsed_datetime = StructureBot.date_from_list(results)
+        unix_ts = StructureBot.to_unix_time(parsed_datetime)
+        timer_map.update({unix_ts: timer_args})
+        #sort the timers and retrieve from the dictionary
+        sorted_timers = timer_map(sorted(dict.items(), reverse=True))
+        timers_msg = '\n'.join([f'> {value} <t:{key}:f> in <t:{key}:R> ID: {key}' for key, value in sorted_timers.items()])
+        return timers_msg
+    
+    def write_to_timers_txt(msg):
+        with open('timers.txt', 'w') as file:
+            file.write(msg)
+        file.close()
